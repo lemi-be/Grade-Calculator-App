cb8a0e8000c78a42417e446a207e8e7c6f68a5f7
lemi-be <lemibekele365@gmail.com>
2024-02-12 22:31:06 +0300

    Added Comments

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+// All the Course Container
 const courseContainer = document.getElementsByClassName('courseContainer');
+// A button to add more subjects
 const addSubjectBtn = document.getElementById('addSubjectBtn');
+// To get the Sudents Grade
 const Grade = document.getElementById('Grade');
+// To get the subjects Credit Hour
 const CreditHour = document.getElementById('creditHr');
+// The Calculate Button
 const calculateBtn = document.getElementById('calculateBtn');
+// This contains all the user input field
 let userInput = document.getElementById('userInput');
  
-
+// function to add more functions
 addSubjectBtn.addEventListener('click', function() {
     let originalCourseDiv = document.querySelector('.courseContainer');
     let clonedCourseDiv = originalCourseDiv.cloneNode(true);
@@ -13,6 +19,7 @@ addSubjectBtn.addEventListener('click', function() {
     
 });
 
+// function to calculate the GPA
 calculateBtn.addEventListener('click', function() {
     
 });
